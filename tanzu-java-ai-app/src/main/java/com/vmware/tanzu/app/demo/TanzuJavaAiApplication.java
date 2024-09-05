5e7682d01c576a4c2ababc364323c7352d9aae36
Ben Hale <ben.hale@broadcom.com>
2024-09-05 15:02:36 -0700

    Ollama Deployment
    Signed-off-by: Ben Hale <ben.hale@broadcom.com>

--- tanzu-java-ai-app/src/main/java/com/vmware/tanzu/app/demo/TanzuJavaAiApplication.java
+++ tanzu-java-ai-app/src/main/java/com/vmware/tanzu/app/demo/TanzuJavaAiApplication.java
@@ -1,12 +1,20 @@
 package com.vmware.tanzu.app.demo;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
 
 @SpringBootApplication
 public class TanzuJavaAiApplication {
@@ -20,5 +28,17 @@ public class TanzuJavaAiApplication {
     @Scope("prototype")
     RestClient.Builder restClientBuilder() {
         return RestClient.builder().requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS));
+    }
+
+    // TODO: Do as I say, not as I do
+    @Bean
+    @Scope("prototype")
+    WebClient.Builder webClientBuilder() throws SSLException {
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient));
     }
 }
